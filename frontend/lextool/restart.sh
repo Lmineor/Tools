7b2561199b45726c0391eb5473a20bbcc2e48611
Lex <luohai2233@163.com>
2020-04-07 16:38:54 +0800

    配置重启脚本

--- frontend/lextool/restart.sh
+++ frontend/lextool/restart.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
+# author:lex<luohai2233@163.com>
 # config variables
-PROJECT_PATH = /www/wwwroot/Tools/frontend/lextool
-PROJECT_NAME = lextools
+PROJECT_BASE_PATH = /www/wwwroot/Tools/frontend/
+PROJECT_NAME = $1
 GIT = git@github.com:Prolht/Tools.git
 
+if [ $PROJECT_NAME ]
+then
+    # stop and pull
+    pm2 stop $PROJECT_NAME
+    git pull
 
-# stop and pull
-pm2 stop $PROJECT_NAME
-cd $PROJECT_PATH
-git pull
+    # install dependence & build
+    cd $PROJECT_BASE_PATH$PROJECT_NAME
+    npm run install
+    npm run build
 
-# install dependence & build
-npm run install
-npm run build
-
-# restart
-pm2 restart $PROJECT_NAME
+    # restart
+    pm2 restart $PROJECT_NAME
+    echo "重启完成"
+else
+    echo "请输入项目名称"
+fi
