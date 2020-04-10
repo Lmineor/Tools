e2d6f21d920fabc89d7041e8ae3bb82c18e63547
Lex <luohai2233@163.com>
2020-04-10 21:44:49 +0800

    clear: clear unused file

--- frontend/lextool/pages/markdown.vue
+++ frontend/lextool/pages/markdown.vue
@@ -2,9 +2,14 @@
     <div class="markdown">
         <nya-container title="markdown">
         </nya-container>
-            <div class="markdown">
-                <Markdown/>
-            </div>
+            <mavon-editor 
+                v-model="content" 
+                ref="md" 
+                @change="change" 
+                style="min-height: 600px"
+            />
+
+        <button @click="submit">提交</button>
         <nya-container v-if="res" title="结果">
             <div v-html="res"></div>
         </nya-container>
@@ -13,14 +18,34 @@
 
 
 <script>
-import MarkdownPro from '@/components/mk/pro';
-import MarkdownPreview from '@/components/mk/preview';
+import { mavonEditor } from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
 
 export default {
-        name: "markdown",
-        components: {
-            MarkdownPro,
-            MarkdownPreview,
+        // 注册
+    components: {
+        mavonEditor,
+    },
+    data() {
+        return {
+            content:'', // 输入的markdown
+            html:'',    // 及时转的html
         }
+    },
+    methods: {
+        // 所有操作都会被解析重新渲染
+        change(value, render){
+            // render 为 markdown 解析后的结果[html]
+            this.html = render;
+        },
+        // 提交
+        submit(){
+            console.log(this.content);
+            console.log(this.html);
+        }
+    },
+    mounted() {
+
     }
+}
 </script>
