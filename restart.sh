b0fbc26df1f992676ec863af6efea70cb72120d1
Lex <luohai2233@163.com>
2020-05-24 12:07:00 +0800

    尝试修复重启脚本

--- restart.sh
+++ restart.sh
@@ -15,34 +15,29 @@ PROJECT_PORT=5000 # backend port
 
 GIT=git@github.com:Prolht/Tools.git
 
-if [$PROJECT_PORT ]
-then
-    # stop backend
-    lsof -i:${PROJECT_PORT}|awk '{print$2}' |grep -v PID|xargs kill -9 # 暂停
+# stop backend
+lsof -i:${PROJECT_PORT}|awk '{print$2}' |grep -v PID|xargs kill -9 # 暂停
 
-    # stop frontend
-    pm2 stop $PROJECT_NAME_F
+# stop frontend
+pm2 stop $PROJECT_NAME_F
 
-    #pull
-    git pull
+#pull
+git pull
 
-    # install dependence & build
-    # backend
-    source activate $CONDA_ENV
-    cd $PROJECT_B_BASE_PATH
-    pip install -r requirements.txt
-    # start
-    nohup python run.py >> server.log 2>&1 &
-    echo "Complete Backend"
+# install dependence & build
+# backend
+source activate $CONDA_ENV
+cd $PROJECT_B_BASE_PATH
+pip install -r requirements.txt
+# start
+nohup python run.py >> server.log 2>&1 &
+echo "Complete Backend"
 
-    # frontend
-    cd $PROJECT_F_BASE_PATH$PROJECT_NAME_F
-    npm install
-    npm run build
+# frontend
+cd $PROJECT_F_BASE_PATH$PROJECT_NAME_F
+npm install
+npm run build
 
-    # start
-    pm2 restart $PROJECT_NAME_F
-    echo "Complete Fronteend"
-else
-    echo "error!!!"
-fi
+# start
+pm2 restart $PROJECT_NAME_F
+echo "Complete Fronteend"
