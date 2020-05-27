44ad5c7248573ec99dcc099f6dc9ce50e3469e81
Lex <luohai2233@163.com>
2020-05-27 17:59:18 +0800

    删除用户名显示

--- frontend/lextool/components/Panel.vue
+++ frontend/lextool/components/Panel.vue
@@ -2,8 +2,11 @@
     <div class="panel" icon="person-outline">
         <div class="login-text">
             <a href="https://github.com/Prolht/Tools" target="_blank" rel="noopener noreferrer" style="color: rgb(0,0,0);">Powered by Lex吐司</a>
-            <a>{{$store.state.user}}</a>
+            <!-- <a>{{$store.state.user}}</a> -->
         </div>
+        <!-- <div class="login-text">
+            <a style="text-align: center; color:rgb(0,0,0);">{{$store.state.user}}</a>
+        </div> -->
     </div>
 </template>
 
