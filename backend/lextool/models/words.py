a29537aaecc9dcf90e266eb86210d7fe209b4a46
Lex <luohai2233@163.com>
2020-06-12 16:00:48 +0800

    debug: 尝试数据库继承错误

--- backend/lextool/models/words.py
+++ backend/lextool/models/words.py
@@ -1,7 +1,9 @@
 from . import db
 
 
-class WordsBaseModel(db.Model):
+class EnWords(db.Model):
+    __bind_key__ = 'words'  # 已设置__bind_key__,则采用设置的数据库引擎
+    __tablename__ = 'enwords'
     id = db.Column(db.Integer, primary_key=True)
     word = db.Column(db.String(32), index=True)
     translation = db.Column(db.String(512), nullable=False)
@@ -9,16 +11,21 @@ class WordsBaseModel(db.Model):
     spellingE = db.Column(db.String(64), nullable=True)  # 英音
 
 
-class EnWords(WordsBaseModel):
-    __bind_key__ = 'words'  # 已设置__bind_key__,则采用设置的数据库引擎
-    __tablename__ = 'enwords'
-
-
-class Cet6(WordsBaseModel):
+class Cet6(db.Model):
     __bind_key__ = 'words'  # 已设置__bind_key__,则采用设置的数据库引擎
     __tablename__ = 'cet6'
+    id = db.Column(db.Integer, primary_key=True)
+    word = db.Column(db.String(32), index=True)
+    translation = db.Column(db.String(512), nullable=False)
+    spellingA = db.Column(db.String(64), nullable=True)  # 美音
+    spellingE = db.Column(db.String(64), nullable=True)  # 英音
 
 
-class Cet4(WordsBaseModel):
+class Cet4(db.Model):
     __bind_key__ = 'words'  # 已设置__bind_key__,则采用设置的数据库引擎
     __tablename__ = 'cet4'
+    id = db.Column(db.Integer, primary_key=True)
+    word = db.Column(db.String(32), index=True)
+    translation = db.Column(db.String(512), nullable=False)
+    spellingA = db.Column(db.String(64), nullable=True)  # 美音
+    spellingE = db.Column(db.String(64), nullable=True)  # 英音
