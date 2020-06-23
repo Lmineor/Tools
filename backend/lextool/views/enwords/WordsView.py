606c1e78398a031cf225f659108818c07175e949
Lex <luohai2233@163.com>
2020-06-23 19:41:21 +0800

    update: 重构接口，并支持每日固定的随机单词

--- backend/lextool/views/enwords/WordsView.py
+++ backend/lextool/views/enwords/WordsView.py
@@ -1,4 +1,5 @@
 import datetime
+from typing import List, Dict
 
 from flask import request
 from flask import Blueprint
@@ -7,54 +8,64 @@ from sqlalchemy.sql.expression import func
 
 from ..user import auth
 from ...logger import logger
-from ...models.words import Cet4, Cet6, TOEFL, GRE
+from ...models.words import *
 from ...models import db
 from ...utils.parseCharacter import parseSpelling
 
 words = Blueprint('words', __name__)
 
 
-@words.route('/daily', methods=['POST', 'GET'])
+@words.route('/daily', methods=['GET'])
 @auth.login_required
 def get_daily_words():
     """
     获取每日单词
+    思路：如果用户请求的日期与数据库中create_at相同，则直接查库返回，否则重新生成新的单词，并存到数据库当中
     """
+    refresh = request.args.get("refresh")  # 若有refresh请求，则刷新
     book = g.user.config.words_book
-    if book == 'CET4':
-        db = Cet4
-    elif book == 'CET6':
-        db = Cet6
-    elif book == 'TOEFL':
-        db = TOEFL
-    else:
-        db = GRE
-    if request.method == 'GET':
-        try:
-            items = db.query.order_by(func.rand()).limit(20)
-            data = [{'word': item.word, 'translation': item.translation, 'spellingA': parseSpelling(item.spellingA), 'spellingE': parseSpelling(item.spellingE)} for item in items]
-        except Exception as e:
-            data = []
-            logger.error(e)
-        return jsonify({
-            'code': 200,
-            'words': data,
-            'book': book
-        })
-    # else:
-    #     chapter = request.get_json()['chapter']
-    #     logger.info('chapter: ' + chapter)
-    #     if cache.get('chapter'):
-    #         paragraphs = cache.get('chapter')
-    #     else:
-    #         try:
-    #             paragraphs = PoemLunyu.query.filter_by(chapter=chapter).first().paragraphs
-    #         except Exception as e:
-    #             paragraphs = ''
-    #             logger.error(e)
-    #         cache.set(chapter, paragraphs)
-    #     return jsonify({
-    #         'code': 200,
-    #         'paragraphs': paragraphs.split('|')
-    #     })
+    words_num = g.user.config.words_num
+    current_date = str(datetime.date.today())
+    logger.info("User {} get {} words".format(g.user.username, book))
 
+    if book == 'CET4':
+        source = Cet4
+    elif book == 'CET6':
+        source = Cet6
+    elif book == 'TOEFL':
+        source = TOEFL
+    else:
+        source = GRE
+    word_items = db.session.query(DailyWords).filter(DailyWords.user_id == g.user.id).all()
+    if not word_items:
+        data = generate_user_daily_words(source, g.user.id, recited=[], words_num=words_num)
+    elif str(word_items[0].create_at)[:10] != current_date or refresh:
+        data = generate_user_daily_words(source, g.user.id, recited=[], words_num=words_num)
+    else:
+        data = [{'word': item.word, 'translation': item.translation, 'spellingA': parseSpelling(item.spellingA),
+                 'spellingE': parseSpelling(item.spellingE)} for item in word_items]
+
+    return jsonify({
+        'code': 200,
+        'words': data,
+        'book': book
+    })
+
+
+def generate_user_daily_words(source, user_id: int, recited: List, words_num: int) -> List[Dict]:
+    try:
+        items = source.query.order_by(func.rand()).limit(words_num)
+        if recited:
+            pass
+        data = [{'word': item.word, 'translation': item.translation, 'spellingA': parseSpelling(item.spellingA),
+                 'spellingE': parseSpelling(item.spellingE)} for item in items]
+        db.session.query(DailyWords).filter(DailyWords.user_id == user_id).delete()  # 删除这个用户之前的所有数据
+        for elem in data:
+            user_d_w = DailyWords(user_id=user_id, word=elem['word'], translation=elem['translation'],
+                                  spellingA=elem['spellingA'], spellingE=elem['spellingE'])
+            db.session.add(user_d_w)
+        db.session.commit()
+    except Exception as e:
+        logger.error("Error is {}".format(e))
+        data = []
+    return data
