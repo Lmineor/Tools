0b6e06cfbd80a466abe8ca5f88cf11f33c54fbb5
Lex <luohai2233@163.com>
2020-06-23 19:38:54 +0800

    update: 重构部分数据库

--- backend/lextool/models/poem.py
+++ backend/lextool/models/poem.py
@@ -1,38 +1,48 @@
 from . import db
 
 
-class PoemSongAuthor(db.Model):
+class PoetIntroduction(db.Model):
     __bind_key__ = 'poem'  # 已设置__bind_key__,则采用设置的数据库引擎
-    __tablename__ = 'songshiauthor'
+    __tablename__ = 'poet_introduction'  # 诗人简介
+
+    id = db.Column(db.Integer, primary_key=True, index=True)
+    descb = db.Column(db.Text(16777216))
+    poet = db.Column(db.String(100))
+    dynasty = db.Column(db.String(8))
+
+
+class PoetSong(db.Model):
+    __bind_key__ = 'poem'  # 已设置__bind_key__,则采用设置的数据库引擎
+    __tablename__ = 'poet_song'
 
     id = db.Column(db.Integer, primary_key=True)
     descb = db.Column(db.Text)
-    name = db.Column(db.String(30))
+    poet = db.Column(db.String(100))
 
 
-class PoemTangAuthor(db.Model):
+class PoetTang(db.Model):
     __bind_key__ = 'poem'  # 已设置__bind_key__,则采用设置的数据库引擎
-    __tablename__ = 'tangshiauthor'
+    __tablename__ = 'poet_tang'
 
     id = db.Column(db.Integer, primary_key=True)
     descb = db.Column(db.Text)
-    name = db.Column(db.String(30))
+    poet = db.Column(db.String(100))
 
 
 class PoemTangSong(db.Model):
     __bind_key__ = 'poem'  # 已设置__bind_key__,则采用设置的数据库引擎
-    __tablename__ = 'tangsongshi'
+    __tablename__ = 'tang_song_poem'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, unique=True, index=True)
     paragraphs = db.Column(db.Text)
-    title = db.Column(db.String(30))
-    author = db.Column(db.String(30))
-    dynasty = db.Column(db.String(10))
+    poem = db.Column(db.Text)
+    poet = db.Column(db.String(100))
+    dynasty = db.Column(db.String(8))
 
 
 class PoemLunyu(db.Model):
     __bind_key__ = 'poem'  # 已设置__bind_key__,则采用设置的数据库引擎
-    __tablename__ = 'lunyu'
+    __tablename__ = 'lun_yu'
 
     id = db.Column(db.Integer, primary_key=True)
     paragraphs = db.Column(db.Text)
@@ -41,30 +51,30 @@ class PoemLunyu(db.Model):
 
 class PoemSongci(db.Model):
     __bind_key__ = 'poem'  # 已设置__bind_key__,则采用设置的数据库引擎
-    __tablename__ = 'songci'
+    __tablename__ = 'song_ci'
 
     id = db.Column(db.Integer, primary_key=True)
     paragraphs = db.Column(db.Text)
     rhythmic = db.Column(db.String(40))
-    author = db.Column(db.String(20))
+    poet = db.Column(db.String(100))
 
 
 class CiAuthor(db.Model):
     __bind_key__ = 'poem'  # 已设置__bind_key__,则采用设置的数据库引擎
-    __tablename__ = 'ciauthor'
+    __tablename__ = 'ci_poet'
 
     id = db.Column(db.Integer, primary_key=True)
     long_desc = db.Column(db.Text)
     short_desc = db.Column(db.Text)
-    name = db.Column(db.String(20))
+    poet = db.Column(db.String(100))
 
 
 class ShiJing(db.Model):
     __bind_key__ = 'poem'  # 已设置__bind_key__,则采用设置的数据库引擎
-    __tablename__ = 'shijing'
+    __tablename__ = 'shi_jing'
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(30))
+    poem = db.Column(db.TEXT)
     chapter = db.Column(db.String(30))
     section = db.Column(db.String(30))
-    content = db.Column(db.String(20))
+    content = db.Column(db.Text)
