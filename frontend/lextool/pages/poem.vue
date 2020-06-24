bca28a80afa81c2e12531c6c6ea9c1afe0ef4682
Lex <luohai2233@163.com>
2020-06-24 15:18:17 +0800

    update:变量格式调整

--- frontend/lextool/pages/poem.vue
+++ frontend/lextool/pages/poem.vue
@@ -4,20 +4,20 @@
             <nya-dropdown
                 v-if="showDynasty"
                 v-model="dynasty"
-                style="width:33%" 
-                :items="dynastys" 
-                label="朝代" 
-                :pageable="false" 
-                v-on:change="getwriters" 
+                style="width:33%"
+                :items="dynastys"
+                label="朝代"
+                :pageable="false"
+                v-on:change="getPoets"
             />
             <nya-dropdown
-                v-if="showWriters"
-                v-model="writer"
+                v-if="showPoets"
+                v-model="poet"
                 style="width:33%"
-                :items="writers"
+                :items="poets"
                 label="诗人"
                 :total="total"
-                v-on:change="getpoems"
+                v-on:change="getPoems"
                 v-on:pagechange="changeAuthorPage"
             />
             <nya-dropdown
@@ -27,17 +27,17 @@
                 :items="poems"
                 label="诗名"
                 :pageable=false
-                v-on:change="getcontent"
+                v-on:change="getContent"
             />
             <div v-if="hasPoem" class="poem">
                 <li class="poem title"><span class="prefix">『</span>{{poem}}<span class="prefix">』</span></li>
-                <li class="poem writer"><span class="prefix">{{dynasty}}·</span>{{writer}}</li>
+                <li class="poem poet"><span class="prefix">{{dynasty}}·</span>{{poet}}</li>
                 <li v-for="item in content" :key="item.index" class="poem content">
                     {{ item }}
                 </li>
             </div>
         </nya-container>
-        
+
     </div>
 </template>
 
@@ -54,16 +54,16 @@ export default {
         return {
             title: '诗歌',
             showDynasty: true,
-            showWriters: false,
+            showPoets: false,
             showPoems: false,
             getDynasty: false,
             hasPoem: false,
             dynastys:['唐','宋'],
-            writers: [],
+            poets: [],
             poems: [],
             poem: '',
             dynasty:'',
-            writer:'',
+            poet:'',
             content: [''],
             loading : false,
             defaultpage: 1,
@@ -76,15 +76,15 @@ export default {
         changeAuthorPage(current){
             this.$axios
                 .post(
-                    envs.apiUrl + '/poem/getauthor',
+                    envs.apiUrl + '/poem/poet/poets',
                     {
                         dynasty: this.dynasty,
                         page: current,
                     },
                 )
                 .then(re => {
-                    this.writers = re.data.authors.sort((a, b) => a.localeCompare(b, 'zh-Hans-CN', {sensitivity: 'accent'})).slice(0,10);
-                    this.showWriters = true;
+                    this.poets = re.data.poets.sort((a, b) => a.localeCompare(b, 'zh-Hans-CN', {sensitivity: 'accent'})).slice(0,15);
+                    this.showPoets = true;
                     this.loading = false;
                 })
                 .catch(err => {
@@ -92,28 +92,28 @@ export default {
                     this.loading = false;
                 });
         },
-        getwriters (id){
+        getPoets (id){
             this.dynasty = this.dynastys[id],
             this.loading = true,
-            this.writers = [],
+            this.poets = [],
             this.poems = [],
             this.poem = '',
-            this.writer = '',
-            this.showWriters = false,
+            this.poet = '',
+            this.showPoets = false,
             this.showPoems = false,
             this.hasPoem = false,
             this.$axios
                 .post(
-                    envs.apiUrl + '/poem/getauthor',
+                    envs.apiUrl + '/poem/poet/poets',
                     {
                         dynasty: this.dynastys[id],
                         page: this.defaultpage,
                     },
                 )
                 .then(re => {
-                    this.writers = re.data.authors.sort((a, b) => a.localeCompare(b, 'zh-Hans-CN', {sensitivity: 'accent'})).slice(0,10);
+                    this.poets = re.data.poets.sort((a, b) => a.localeCompare(b, 'zh-Hans-CN', {sensitivity: 'accent'})).slice(0,15);
                     this.total = re.data.total;
-                    this.showWriters = true;
+                    this.showPoets = true;
                     this.loading = false;
                 })
                 .catch(err => {
@@ -121,22 +121,22 @@ export default {
                     this.loading = false;
                 });
         },
-        getpoems (id){
-            this.writer = this.writers[id],
+        getPoems (id){
+            this.poet = this.poets[id],
             this.poems = [],
             this.poem = '',
             this.hasPoem = false,
             this.showPoems = false,
             this.$axios
                 .post(
-                    envs.apiUrl + '/poem/gettitle',
+                    envs.apiUrl + '/poem/poet/poems',
                     {
                         dynasty: this.dynasty,
-                        author: this.writer
+                        poet: this.poet
                     },
                 )
                 .then(re => {
-                    this.poems = re.data.titles.sort((a, b) => a.localeCompare(b, 'zh-Hans-CN', {sensitivity: 'accent'}));
+                    this.poems = re.data.poems.sort((a, b) => a.localeCompare(b, 'zh-Hans-CN', {sensitivity: 'accent'}));
                     this.showPoems = true
                     this.loading = true;
                 })
@@ -145,19 +145,19 @@ export default {
                     this.loading = false;
                 });
         },
-        getcontent(id){
+        getContent(id){
             this.poem = this.poems[id]
             this.$axios
                 .post(
-                    envs.apiUrl + '/poem/getpoem',
+                    envs.apiUrl + '/poem/poet/content',
                     {
                         dynasty: this.dynasty,
-                        author: this.writer,
-                        title: this.poem
+                        poet: this.poet,
+                        poem: this.poem
                     },
                 )
                 .then(re => {
-                    this.content = re.data.poem.split('。'),
+                    this.content = re.data.content;
                     this.hasPoem = true
                 })
                 .catch(err => {
@@ -185,7 +185,7 @@ export default {
             font-weight: 100;
         }
         font-family: "楷体","楷体_GB2312";
-        
+
     }
 }
 
