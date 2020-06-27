9f067f8c63c3403edeeb29a37308e199849577fd
Lex <luohai2233@163.com>
2020-06-27 14:32:18 +0800

    update:url字段长度修改

--- backend/lextool/models/dwz.py
+++ backend/lextool/models/dwz.py
@@ -10,6 +10,6 @@ class DWZ(db.Model):
     __bind_key__ = 'dwz'  # 已设置__bind_key__,则采用设置的数据库引擎
     __tablename__ = 'dwz'  # 未设置__bind_key__,则采用默认的数据库引擎
     id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.String(80), unique=True)
+    url = db.Column(db.String(255), unique=True)
     dwz = db.Column(db.String(10), unique=True, index=True)
     create_at = db.Column(db.DateTime, default=datetime.datetime.now)
