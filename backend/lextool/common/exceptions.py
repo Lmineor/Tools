9033fb6936a90e69f3618db9e38cc7d3942c5442
Lex <luohai2233@163.com>
2020-12-28 22:50:37 +0800

    update exception and increase some exception

--- backend/lextool/common/exceptions.py
+++ backend/lextool/common/exceptions.py
@@ -2,44 +2,32 @@ from werkzeug.exceptions import HTTPException, Forbidden
 
 
 class ToolsBaseError(Exception):
-    """Root exception for """
-    pass
-
-
-class ToolsHTTPError(ToolsBaseError, HTTPException):
+    """Root exception for tools"""
     description = "An internal error has occured"
-
-
-ToolsError = ToolsHTTPError
-
-
-class AuthorizationRequired(ToolsError, Forbidden):
-    description = "Authorization is required."
-
-
-class AuthenticationError(ToolsError):
-    description = "Invalid username and password combination."
-
-
-# Config Error
-class ConfigBaseError(Exception):
-    def __init(self, msg):
+    
+    def __init__(self, msg = None):
         self.msg = msg
         
     def __str__(self):
+        if self.msg is None:
+            return self.description
         return self.msg
 
 
-class ConfigFileNotFoundError(ConfigBaseError):
+class AuthorizationRequired(ToolsBaseError, Forbidden):
+    description = "Authorization is required."
+
+
+class ConfigFileNotFoundError(ToolsBaseError):
     """Raised if config file  does not exist"""
     def __init__(self, config_dir):
         self.config_dir = config_dir
     
     def __str__(self):
-        return ('Failed to read config file directory: %s' % self.config_dir)
+        return ('Failed to load config file in the directory: %s' % self.config_dir)
 
 
-class NoSuchGroupError(ConfigBaseError):
+class NoSuchGroupError(ToolsBaseError, AttributeError):
     """Raised if config does not has such group"""
     def __init__(self, group_name):
         self.group_name = group_name
@@ -48,21 +36,24 @@ class NoSuchGroupError(ConfigBaseError):
         return "no such group [%s]" % self.group_name
 
 
-class NoSuchOptError(ConfigBaseError, AttributeError):
+class NoSuchOptError(ToolsBaseError, AttributeError):
     """Raised if config does not has such option"""
-    def __init__(self, opt_name, group=None):
+    def __init__(self, opt_name, group):
         self.opt_name = opt_name
         self.group = group
 
     def __str__(self):
-        group_name = "DEFAULT" if self.group is None else self.group.name
-        return "no such option %s in group [%s]" % (self.opt_name, group_name)
-    
-    
-class ConfigFileValueError(ConfigBaseError, ValueError):
-    """Raised if a config file value does not match its opt type"""
-    pass
+        return "no such option %s in group [%s]" % (self.opt_name, self.group)
 
+class InvaildOption(ToolsBaseError):
+    def __init__(self, k, v):
+        self.k = k
+        self.v = v
+    
+    def __str__(self):
+        return "The group name [%s] or option %s is invalid" % (self.k, self.v)
 
-class InvaildOption(ConfigBaseError):
-    pass
+class PoemNotFound(ToolsBaseError, HTTPException):
+    description = "The poem you refer does not exist, check your input"
+    code = 404
+    response = 404
