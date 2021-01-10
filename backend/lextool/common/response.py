c82100596b36bc163cb9cd31b790311ebcb7ff41
Lex <luohai2233@163.com>
2021-01-10 21:32:37 +0800

    增加了注释

--- backend/lextool/common/response.py
+++ backend/lextool/common/response.py
@@ -11,6 +11,10 @@ from .constants import SUCCESS_CODE, NOTFOUND_CODE
 
 
 def not_found_resp(data):
+    """
+    the func of fail
+    data: the data to return
+    """
     return jsonify({
         'code': NOTFOUND_CODE,
         'description': 'The info you request does not exist...',
@@ -18,6 +22,12 @@ def not_found_resp(data):
     })
         
 
-def success_resp(data):
-    return jsonify({'code': SUCCESS_CODE, 'data': data})
-    
+def success_resp(req_info, data):
+    """
+    the func of success
+    req_info: request info
+    data: the data to return
+    """
+    resp_body = req_info
+    resp_body.update(data)
+    return jsonify({'code': SUCCESS_CODE, 'data': resp_body})
